deeda7ad0a48e6b448aef16ffdb15acc8ee92bc6
[Ankita Soni] <[Ankitasoni2211104@gmail.com]>
2024-02-05 14:22:09 +0530

    React hooks revised

--- src/components/ReturantCard.js
+++ src/components/ReturantCard.js
@@ -1,16 +1,28 @@
-const RestaurantCard = (p) => {
-  console.log(p);
+const RestaurantCard = (props) => {
+  console.log(props);
+  const { resData } = props;
+  const {
+    name,
+    cuisines,
+    avgRating,
+    costForTwo,
+    deliveryTime,
+    cloudinaryImageId
+
+  } = resData?.data;
   return (
     <div className="res-card">
       <img
         className="res-image"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq88d1YIgUuvNoh8mON8iTYrd_LEjNmSWV995iA1tCmb4M7rMdoEQ0775zmsfF7NjbeDc&usqp=CAU"
+        src={
+          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+cloudinaryImageId
+        }
         alt="res image"
       />
-      <h3>{p.resName}</h3>
-      <h4>{p.cusine}</h4>
-      <h4>4.4 stars</h4>
-      <h4>20 mins</h4>
+      <h3>{name}</h3>
+      <h4>{cuisines}</h4>
+      <h4>{avgRating}</h4>
+      <h4>{deliveryTime}</h4>
     </div>
   );
 };
