c17d1c472d5385fdda21837a1a5d6a06e145e961
[Ankita Soni] <[Ankitasoni2211104@gmail.com]>
2024-02-06 15:28:21 +0530

    shimmer ui added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,27 +1,33 @@
-import resList from "../../utils/mockdata";
 import RestaurantCard from "./ReturantCard";
 import { useEffect, useState } from "react";
-import resList from "../../utils/mockdata";
+import ShimmerUi from "./ShimmerUi";
 const Body = () => {
   // State variable(super powered react variable(by using hooks))
   //   const [listOfRestaurants, listOfRestaurants] = useState(resList);
   // same way array destucturing
-  const arr = useState(resList);
+  const arr = useState([]);
   const [listOfRestaurants, setlistOfRestaurants] = arr;
   useEffect(() => {
     fetchData();
   }, []);
+
   const fetchData = async () => {
     const data = await fetch(
       "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.65420&lng=77.23730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
 
     const json = await data.json();
-    console.log(json);
+
+    // Optinal Chaining
     setlistOfRestaurants(
-      json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants
+      json?.data?.cards[1]?.card?.card?.gridElements.infoWithStyle.restaurants
     );
   };
+
+  // conditional rendering
+  if (listOfRestaurants.length === 0) {
+    return <ShimmerUi />;
+  }
 
   return (
     <div className="body">
@@ -42,14 +48,13 @@ const Body = () => {
             setlistOfRestaurants((res) =>
               res.filter((resturant) => resturant.info.costForTwo > 200)
             );
-            console.log(listOfRestaurants);
           }}>
           Affordable meals
         </button>
       </div>
 
       <div className="restro-card">
-        {listOfRestaurants.map((restaurant ,index) => (
+        {listOfRestaurants.map((restaurant, index) => (
           <RestaurantCard key={index} resData={restaurant} />
         ))}
       </div>
---
+++ src/components/ShimmerUi.js
@@ -0,0 +1,19 @@
+const ShimmerUi = () => {
+  return (
+    <div className="shimmer-container">
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+      <div className="shimmer-card"></div>
+    </div>
+  );
+};
+export default ShimmerUi;
