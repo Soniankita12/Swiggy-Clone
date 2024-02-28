c65fe65d063e708e21903bf3eee562cfde7dd6bf
soniankita12 <ankitasoni2211104@gmail.com>
2024-02-28 23:52:00 +0530

    grouped test case

--- src/components/__Tests__/ContactUs.test.js
+++ src/components/__Tests__/ContactUs.test.js
@@ -2,16 +2,49 @@ import { render, screen } from "@testing-library/react";
 import ContactUs from "../ContactUs";
 import "@testing-library/jest-dom";
 
-test("should load contact us component",()=>{
-render(<ContactUs/>);
-const heading = screen.getByRole("heading");
-//Assertion
-expect(heading).toBeInTheDocument();
+describe('', () => {
+    test("should load contact us component", () => {
+      render(<ContactUs />);
+      const heading = screen.getByRole("heading");
+      //Assertion
+      expect(heading).toBeInTheDocument();
+    });
+
+    test("should load contact us component", () => {
+      render(<ContactUs />);
+      const butto = screen.getByPlaceholderText("Message");
+      //Assertion
+      expect(butto).toBeInTheDocument();
+    });
+
+    test("should load 2 input box", () => {
+      render(<ContactUs />);
+      const inputbtn = screen.getAllByRole("textbox");
+      //Assertion
+      expect(inputbtn.length).toBe(2);
+    });
+    
+});
+
+
+test("should load contact us component", () => {
+  render(<ContactUs />);
+  const heading = screen.getByRole("heading");
+  //Assertion
+  expect(heading).toBeInTheDocument();
 });
 
 test("should load contact us component", () => {
   render(<ContactUs />);
-  const button = screen.getByText("Submit");
+  const butto = screen.getByPlaceholderText("Message");
   //Assertion
-  expect(button).toBeInTheDocument();
+  expect(butto).toBeInTheDocument();
+});
+
+test("should load 2 input box",()=>{
+render(<ContactUs/>);
+const inputbtn = screen.getAllByRole("textbox");
+//Assertion   
+expect(inputbtn.length).toBe(2);
+
 });
