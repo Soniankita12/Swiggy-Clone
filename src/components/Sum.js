e6303dcc80a44ffafabf7c0312dfc2c0fa235a64
soniankita12 <ankitasoni2211104@gmail.com>
2024-03-05 22:47:27 +0530

    deployed

--- src/components/Sum.js
+++
@@ -1,3 +0,0 @@
-export const Sum = (a,b)=>{
-    return a+b ;
-}
--- src/components/__Tests__/Sum.test.js
+++
@@ -1,7 +0,0 @@
-import { Sum } from "../Sum";
-
-test("sum function should calculate the sum of two number", () => {
-  const res = Sum(13, 7);
-  //Assertion
-  expect(res).toBe(20);
-});
